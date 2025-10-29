34169b79a4e6673c1fc42c8bc604259880578dcf
dyerman456 <dyerman456@gmail.com>
2025-10-29 06:33:52 +0100

    feat: add Main page button in NotFound component

--- src/common/components/NotFound/NotFound.tsx
+++ src/common/components/NotFound/NotFound.tsx
@@ -1,10 +1,21 @@
 import s from "./NotFound.module.css"
+import Button from "@mui/material/Button"
+import { Path } from "@/common/routing"
+import { Link } from "react-router"
 
 export const NotFound = () => {
   return (
     <>
       <h1 className={s.title}>404</h1>
       <h2 className={s.subtitle}>page not found</h2>
+      <Button
+        component={Link}
+        to={Path.Main}
+        variant={"contained"}
+        style={{ width: "200px", padding: "15px", margin: "0 auto" }}
+      >
+        Main page
+      </Button>
     </>
   )
 }
